37e13f95c98c42cc5a015b22f244bf490f5ca9b0
bnula <bnulagnula@gmail.com>
2021-04-17 20:14:58 +0200

    add functionality to add items

--- app.js
+++ app.js
@@ -37,14 +37,21 @@ mongoose.connect(mongoUrl, {
 
 const userSchema = mongoose.Schema({
    userName: String,
+   name: String,
    password: String,
    googleId: String
+});
+
+const secretSchema = mongoose.Schema({
+   author: userSchema,
+   content: String
 });
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
+const Secret = mongoose.model("Secret", secretSchema);
 
 passport.use(User.createStrategy());
 passport.serializeUser(function(user, done) {
@@ -63,7 +70,7 @@ passport.use(new GoogleStrategy({
    callbackURL: "http://localhost:4000/auth/google/secret"
  },
  function(accessToken, refreshToken, profile, cb) {
-   User.findOrCreate({ googleId: profile.id }, function (err, user) {
+   User.findOrCreate({ googleId: profile.id, name: profile.displayName }, function (err, user) {
      return cb(err, user);
    });
  }
@@ -90,7 +97,7 @@ app.route("/register")
       const body = req.body;
       const userName = body.username;
       const password = body.password;
-      User.register({username: userName, active: false}, password, (err, user) => {
+      User.register({username: userName, name: userName, active: false}, password, (err, user) => {
          if (err) {
             console.log(err);
          };
@@ -126,10 +133,41 @@ app.route("/login")
 app.route("/secrets")
    .get((req, res) => {
       if (req.isAuthenticated()) {
-         res.render("secrets");
+         Secret.find((err, data)=>{
+            if (err) {
+               console.log(err);
+            } else {
+               res.render("secrets", {secrets: data});
+            };
+         });
       } else {
          res.redirect("/login");
       };
+   });
+
+app.route("/submit")
+   .get((req, res) => {
+      if (req.isAuthenticated()) {
+         res.render("submit");
+      } else {
+         res.redirect("/login");
+      };
+   })
+   .post((req, res) => {
+      const content = req.body.secret;
+      const authorId = req.session.passport.user;
+      User.findById(authorId, async (err, author) => {
+         if (err) {
+            console.log(err);
+         } else {
+            secret = new Secret({
+               author: author,
+               content: content
+            });
+            await secret.save();
+            res.redirect("/secrets");
+         };
+      });
    });
 
 app.route("/logout")
