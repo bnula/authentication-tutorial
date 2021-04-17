379cfe9a75e3da20fb5d4b32acf9048d1b11fc62
bnula <bnulagnula@gmail.com>
2021-04-17 13:01:37 +0200

    change hashing to bcrypt

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const port = 4000;
 
 const _ = require("lodash");
 
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const mongoose = require("mongoose");
 
@@ -35,30 +36,6 @@ app.get("/", (req, res) => {
    res.render("home");
 });
 
-app.route("/login")
-   .get((req, res) => {
-      res.render("login");
-   })
-   .post(async (req, res) => {
-      const body = req.body;
-      const userName = (body.username).toUpperCase();
-      const password = md5(body.password);
-      await User.findOne({userName: userName}, (err, user) => {
-         if (err) {
-            console.log(err);
-         } else {
-            if (user) {
-               if (password === user.password) {
-                  res.render("secrets");
-               };
-            }
-            else {
-               res.redirect("login");
-            };
-         };
-      });
-   });
-
 app.route("/register")
    .get((req, res) => {
       res.render("register");
@@ -66,17 +43,54 @@ app.route("/register")
    .post(async (req, res) => {
       const body = req.body;
       const userName = (body.username).toUpperCase();
-      const password = md5(body.password);
-      const newUser = new User ({
-         userName: userName,
-         password: password
-      });
-      await newUser.save((err) => {
-         if(err) {
-            console.log(err)
+      bcrypt.hash(body.password, saltRounds, async (err, hash) => {
+         if (err) {
+            console.log(err);
+         } else {
+            const newUser = new User ({
+               userName: userName,
+               password: hash
+            });
+            await newUser.save((err) => {
+               if(err) {
+                  console.log(err)
+               };
+            });
          };
       });
       res.render("secrets");
+   });
+
+app.route("/login")
+   .get((req, res) => {
+      res.render("login");
+   })
+   .post(async (req, res) => {
+      const body = req.body;
+      const userName = (body.username).toUpperCase();
+      const password = body.password;
+      await User.findOne({userName: userName}, (err, user) => {
+         if (err) {
+            console.log(err);
+         } else {
+            if (user) {
+               bcrypt.compare(password, user.password, (err, result) => {
+                  if (err) {
+                     console.log(err);
+                  } else {
+                     if (result) {
+                        res.render("secrets");
+                     } else {
+                        res.redirect("login");
+                     };
+                  };
+               });
+            }
+            else {
+               res.redirect("login");
+            };
+         };
+      });
    });
 
 app.listen(port, () => {
